2b6fa44a4c5f0924eaa2df2c3956736ef3b5375e
zhangenzhi <zhangenzhi657@gmail.com>
2024-09-30 16:45:54 +0900

    test pe channel.

--- model/sam2_mod/hieradet.py
+++ model/sam2_mod/hieradet.py
@@ -201,10 +201,10 @@ class Hiera(nn.Module):
         self.q_pool_blocks = [x + 1 for x in self.stage_ends[:-1]][:q_pool]
         self.return_interm_layers = return_interm_layers
 
-        self.patch_embed = PatchEmbed(
-            embed_dim=embed_dim,
-            in_chans = in_chans,
-        )
+        # self.patch_embed = PatchEmbed(
+        #     embed_dim=embed_dim,
+        #     in_chans = in_chans,
+        # )
         # self.patch_embed_half = PatchEmbed(
         #     embed_dim=embed_dim,
         #     in_chans = in_chans,
@@ -213,13 +213,13 @@ class Hiera(nn.Module):
         #     padding=(0,0)
         # )
         
-        # self.patch_embed_quad = PatchEmbed(
-        #     embed_dim=embed_dim,
-        #     in_chans = in_chans,
-        #     stride=(1,1),
-        #     kernel_size=(1,1),
-        #     padding=(0,0)
-        # )
+        self.patch_embed_quad = PatchEmbed(
+            embed_dim=embed_dim,
+            in_chans = in_chans,
+            stride=(1,1),
+            kernel_size=(1,1),
+            padding=(0,0)
+        )
         # Which blocks have global att?
         self.global_att_blocks = global_att_blocks
 
